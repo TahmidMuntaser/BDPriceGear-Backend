a25d5ebcda7359791ad9230c153ed65ceb046877
Tahmid <tahmid25muntaser@gmail.com>
2025-08-12 19:48:31 +0600

    ADD: a static scrapper function is added for ultratech

--- bdpricegear-backend/products/scraper.py
+++ bdpricegear-backend/products/scraper.py
@@ -111,7 +111,6 @@ def scrape_skyland(product):
     
 
 # pchouse 
-
 def scrape_pchouse(product):
     try:
         url = f"https://www.pchouse.com.bd/index.php?route=product/search&search={urllib.parse.quote(product)}"
@@ -148,8 +147,43 @@ def scrape_pchouse(product):
         return {"products": [], "logo": "logo not found"}
     
 
-def scrape_ultratech(product):
-    return {"products": [], "logo": ""}
+# ultratech 
+def scrape_techland(product):
+    try:
+        url = f"https://www.ultratech.com.bd/index.php?route=product/search&search={urllib.parse.quote(product)}"
+        response = requests.get(url, timeout = 15)
+        soap = BeautifulSoup(response.text, "html.parser")
+        
+        products = []
+        
+        logo = soap.select_one("#logo img")
+        if logo:
+            logo_url = logo["src"]
+        else:
+            logo_url = "logo not found"
+            
+        for item in soap.select(".product-layout"):
+            name = item.select_one(".name")
+            price = item.select_one(".price-new")
+            img = item.select_one(".product-img img")
+            link = item.select_one(".product-img")
+            
+            products.append({
+                    "id": str(uuid.uuid4()),
+                    "name": name.text.strip() if name else "Name not found",
+                    "price": normalize_price(price.text.strip()) if price else "Out Of Stock",
+                    "img": img["src"] if img else "Image not found",
+                    "link": link["href"] if link else "Link not found"
+            })
+            
+        return {"products": products, "logo": logo_url}
+    
+    except Exception as e:
+        
+        logger.error(f"UltraTech error: {e}")
+        return {"products": [], "logo": "logo not found"}
+    
+    
 
 def scrape_binary(product):
     return {"products": [], "logo": ""}
