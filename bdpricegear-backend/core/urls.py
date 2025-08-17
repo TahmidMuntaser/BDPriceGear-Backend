84772d22eec742a28a3baf58cd80c5e372b3f1de
Tahmid <tahmid25muntaser@gmail.com>
2025-08-18 00:12:12 +0600

    Add root URL handler

--- bdpricegear-backend/core/urls.py
+++ bdpricegear-backend/core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
+     path('', lambda request: HttpResponseRedirect('/api/price-comparison/')), 
 ]
